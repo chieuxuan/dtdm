52368c1f81af0e85bf42f95b328f3ca41f1660bb
Trần Hạnh <97442873+tranhanhh@users.noreply.github.com>
2023-09-06 10:48:29 +0700

    Update bai1.py

--- bai1.py
+++ bai1.py
@@ -1,45 +1,7 @@
-a = 10
-b = 11.5
-
-def sum();
-
-
-#khai bao ham trong python
-def tinhtong(x,y);
-    return x+y;
-#tinhtong(3,4);
-print("tong cua 2 so x va y la: ", tinhtong(3,4));
-x = int(input('nhap vao gia tri cua x: '));
-y = int(input('nhap vao gia tri cua y: '));
-print('tong cua 2 so x va y la: ', tinhtong(x,y));
-if(x<y);
-    print('x nho hon y');
-elif(x=y):
-    print('x bang y');
-elif(x>y):
-    print('x lon hon y');
-s = input('nhap vao chuoi: ');
-for item in range(0,len(s)):
-    print(i);
-#nhap vao so sinh vien trong mot lop tu ban phim
-#nhap vao so sinh vien trong mot lop tu ban phim
-n = int(input("nhap vao so sinh vien trong mot lop: "));
-sv = [];
-msv = [];
-hoten = [];
-for i in range (0,n):
-    print("nhap thong tin cho sinh vien thu ", i,": ");
-    ms = input("nhap ma sinh vien thu");
-    #msv.append(ms);
-    ht = input('nhap vao ho ten');
-    #hoten.append(ht);
-    sv.append('ma sv: ' + ms + 'ho ten: '+ ht);
-for i in range(0,sv.len()):
-    print(i);
-
-
-#nhap thong tin cho tung sinh vien tu ban phim (ho ten, msv)
-#in ra danh sach sinh vien
-
-
-#cai dat pygame
+a = input('Nhập vào họ tên:')
+nam_sinh = int(input('Nhập vào năm sinh:'))
+#hiển thị kết quả:
+import datetime
+year = datetime.datetime.now().year
+tuoi = year - nam_sinh
+print('Bạn \"', a.upper(), '\"năm nay', tuoi, 'tuổi!')
